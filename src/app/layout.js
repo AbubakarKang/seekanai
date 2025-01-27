d14ee5b763ccefded4a96188a080e0298b66aef4
hackie <kangabubakar33@gmail.com>
2025-01-27 20:34:00 +0800

    favicon fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Seek An AI | Find The Right AI For You',
   description: 'Seek An AI will find the right AI tool for you that you can use to streamline your work and tasks. Get specialized AI to perform the task for you',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
