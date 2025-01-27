3bb70d0f9de3c0553c3058f327cccb369952980d
hackie <kangabubakar33@gmail.com>
2025-01-27 20:39:25 +0800

    favicon fix 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
   title: 'Seek An AI | Find The Right AI For You',
   description: 'Seek An AI will find the right AI tool for you that you can use to streamline your work and tasks. Get specialized AI to perform the task for you',
   icons: {
-    icon: '/favicon.png',
+    icon: '/favicon.ico',
   },
 }
 
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
